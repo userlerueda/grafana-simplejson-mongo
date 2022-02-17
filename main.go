3b3b654a16f35d3b89aee0301af98f6380632448
Luis Rueda <userlerueda@users.noreply.github.com>
2022-02-17 11:16:06 -0500

    [feat] Add support for reading variables from env

--- main.go
+++ main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"log"
+	"os"
+	"strconv"
 
 	"github.com/m-masataka/grafana-simplejson-mongo/api"
 )
 
 func main() {
+	p, err := strconv.Atoi(os.Getenv("GSM_PORT"))
+	if err != nil {
+		p = 8080
+	}
+	m := os.Getenv("GMS_MONGOHOST")
+	if m == "" {
+		m = "localhost"
+	}
 	conf := api.Config{
-		Port:      8080,
-		MongoHost: "localhost",
+		Port:      p,
+		MongoHost: m,
 	}
 	errs := make(chan error, 2)
 	api.StartHTTPServer(conf, errs)
